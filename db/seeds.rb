83fa2423a3a57fbc0d20ba059c2a2b626ffc2e32
kenk11 <hoangvu23108@gmail.com>
2016-07-25 00:42:19 +0700

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,6 +24,8 @@ e = Event.create({
   name: 'Việt Nam Thử Thách Chiến Thắng', 
   starts_at: DateTime.parse('Thu, 11 Aug 2016 7:00 AM+0700'),
   ends_at: DateTime.parse('Sat, 13 Aug 2016 3:00 PM+0700'),
+  author_id: 1,
+  publish: false,
   venue: dalat,
   category: Category.find_by(name: 'Everything Else'),
   hero_image_url: 'https://az810747.vo.msecnd.net/eventcover/2015/10/25/C6A1A5.jpg?w=1040&maxheight=400&mode=crop&anchor=topcenter',
@@ -51,6 +53,8 @@ dan_venue = Venue.create({
 e = Event.create({
   name: 'Cảm ơn Đời - Live Concert Đan Trường', 
   venue: dan_venue,
+  author_id: 1,
+  publish: false,
   category: Category.find_by(name: 'Entertainment'),
   starts_at: DateTime.parse('Sat, 16 Jan 2016, 8:00 PM+0700'),
   ends_at: DateTime.parse('Sat, 16 Jan 2016, 10:30 PM+0700'),  
@@ -136,6 +140,8 @@ e = Event.create({
   starts_at: DateTime.parse('Thu, 24 Dec 2015, 8:00 PM+0700'),
   ends_at: DateTime.parse('Thu, 24 Dec 2015, 11:00 PM+0700'),
   venue: gap,
+  author_id: 1,
+  publish: false,
   category: Category.find_by(name: 'Entertainment'),
   hero_image_url:'https://az810747.vo.msecnd.net/eventcover/2015/12/12/78534E.jpg?w=1040&maxheight=400&mode=crop&anchor=topcenter',
   extended_html_description: <<-DESC
