1d5e54211c5da4527ebd6799f805952e7b3960b5
kenk11 <hoangvu23108@gmail.com>
2016-07-25 18:17:45 +0700

    update view can't booking

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,7 +1,11 @@
 class TicketsController < ApplicationController
   skip_before_action :require_login, only: [:new]
+
   def new
     @event = Event.find(params[:event_id])
+    unless @event.is_coming?
+      flash[:error] = 'Event finished! Booking is unavailable!'
+    end
   end
 
   def create
